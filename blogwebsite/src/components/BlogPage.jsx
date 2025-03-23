f97e656d3d565ae6e2ad3e5802fea92d91b494b3
subhrajyoti11 <goudabalunkeswar@gmail.com>
2025-03-23 11:38:03 +0530

    react projects

--- blogwebsite/src/components/BlogPage.jsx
+++ blogwebsite/src/components/BlogPage.jsx
@@ -2,17 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Blogpage = () => {
     const [blogs, setBlogs] = useState([]);
-    useEffect(() => {
-        async function fetchBlogs() {
-            let url = `http://localhost:5000/blogs`;
-            const response = await fetch(url);
-            const data = await response.json();
     
-            setBlogs(data);
-        }
-        fetchBlogs();
-    }, [])
-
     
     return (
         <div>Page category</div>
